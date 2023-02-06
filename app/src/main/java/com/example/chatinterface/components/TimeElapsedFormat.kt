1f06dce3162d1108d24fe0e79b137e1437da2de0
SalatielGenol <salatiel@genol.es>
2023-02-06 05:56:17 +0100

    Añadida funcion que formatea la marca de tiempo

---
+++ app/src/main/java/com/example/chatinterface/components/TimeElapsedFormat.kt
@@ -0,0 +1,27 @@
+package com.example.chatinterface.components
+
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+fun getTimeElapsedFormat(date: Date): String{
+    val currentTime = Calendar.getInstance().time
+    return when (getDifferenceTimeDefinedByUnit(currentTime, date, 's')){
+        in 0..59 -> getDifferenceTimeDefinedByUnit(currentTime, date, 's').toString() + " s"
+        in 60..3599 -> getDifferenceTimeDefinedByUnit(currentTime, date, 'm').toString() + " m"
+        in 3600..86399 -> getDifferenceTimeDefinedByUnit(currentTime, date, 'h').toString() + " h"
+        else -> SimpleDateFormat("d MMM", Locale.getDefault()).format(date)
+    }
+}
+
+private fun getDifferenceTimeDefinedByUnit(initialTime: Date, finalTime: Date, timeUnit: Char): Long{
+    val initialTimeLong = initialTime.time
+    val finalTimeLong = finalTime.time
+    return when (timeUnit){
+        's' -> TimeUnit.MILLISECONDS.toSeconds(initialTimeLong - finalTimeLong)
+        'm' -> TimeUnit.MILLISECONDS.toMinutes(initialTimeLong - finalTimeLong)
+        'h' -> TimeUnit.MILLISECONDS.toHours(initialTimeLong - finalTimeLong)
+        'd' -> TimeUnit.MILLISECONDS.toDays(initialTimeLong - finalTimeLong)
+        else -> Calendar.getInstance().timeInMillis
+    }
+}
