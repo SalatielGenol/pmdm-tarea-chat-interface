30e296bc84fadd6cb531ed2d960a4cabc2ac3516
Salatiel Genol <salatiel@genol.es>
2023-02-16 17:59:21 +0100

    Añadido archivo independiente para tema personalizado

---
+++ app/src/main/java/com/example/chatinterface/ui/theme/CustomColors.kt
@@ -0,0 +1,10 @@
+package com.example.chatinterface.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+data class CustomColors (
+    val material: Colors,
+    val senderCardBackground: Color,
+    val receiverCardBackground: Color
+)
